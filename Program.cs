73a5837756a3071062793fd9e594f73cbb7122bf
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-24 16:37:37 +0530

    greeting msg

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using AddressBookManagementSystem;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AddressBookManagementSystem
+{
+    class AdressBookMainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("!!!!!!Welcome to Adress Book Program!!!!!!");
+            AddPerson contact = new AddPerson();
+            contact.details();
+        }
+    }
+}
